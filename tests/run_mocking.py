61ae48f7939cb79cf3fcb0f7f9887bdc4e410751
jansea92 <alina.jansen@fu-berlin.de>
2024-03-10 18:46:56 +0100

    prep runtime

--- tests/run_mocking.py
+++ tests/run_mocking.py
@@ -1,13 +1,15 @@
 import sys
 import os
+import cProfile
+import pstats
 
 gmx2qmmm_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..\..'))
 integration_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(gmx2qmmm_path)
 sys.path.append(integration_path)
 
-# from gmx2qmmm.operations.qmmm_job import execute_g16, execute_gmx
-
+from gmx2qmmm.operations.qmmm_job import execute_g16, execute_gmx
+from IntegrationTest import IntegrationTest
 
 '''
 -----------------------------------------------------------------
@@ -15,14 +17,20 @@ Insert path to folder here:
 -----------------------------------------------------------------
 '''
 directory = os.path.join(os.path.dirname(__file__), 'temp_bfgs_cp24')
+prof_file = 'profiling_bfgs_cp24.prof'
+txt_file = 'profiling_bfgs_cp24.txt'
 '''
 -----------------------------------------------------------------
 '''
 
-from IntegrationTest import IntegrationTest
-
 IntegrationTest.execute_gmx2qmmm_mocking(directory)
-pass
 
+cProfile.run("IntegrationTest.execute_gmx2qmmm_mocking(directory)", filename=os.path.join(directory, prof_file))
+with open(os.path.join(directory, txt_file), "w") as output_file:
+    stats = pstats.Stats(os.path.join(directory, prof_file), stream=output_file)
+    stats.sort_stats('cumtime')
+    stats.print_stats(100)
+
+print(f"Profiling analysis saved to {txt_file}")
 
 # Next step: untar step2!
