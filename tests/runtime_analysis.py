9dc3fdc90aa4c49d9aa642d77e3d2c44f18b9475
jansea92 <alina.jansen@fu-berlin.de>
2024-03-10 18:26:36 +0100

    working on combining mocking and bfgs

--- tests/runtime_analysis.py
+++ tests/runtime_analysis.py
@@ -11,20 +11,10 @@ from IntegrationTest import IntegrationTest
 
 # set working directory
 test_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-prof_file = 'profiling_bfgs_cp24.prof'
-txt_file = 'profiling_bfgs_cp24.txt'
+prof_file = 'profiling_optimization.prof'
+txt_file = 'profiling_optimization.txt'
 
-'''
------------------------------------------------------------------
-Insert path to folder here:
------------------------------------------------------------------
-'''
-directory = os.path.join(os.path.dirname(__file__), 'temp_bfgs_cp24')
-'''
------------------------------------------------------------------
-'''
-
-cProfile.run("IntegrationTest.execute_gmx2qmmm_mocking(directory)", filename=os.path.join(test_path, prof_file))
+cProfile.run("IntegrationTest.execute_gmx2qmmm_mocking()", filename=os.path.join(test_path, prof_file))
 with open(os.path.join(test_path, txt_file), "w") as output_file:
     stats = pstats.Stats(os.path.join(test_path, prof_file), stream=output_file)
     stats.sort_stats('cumtime')
