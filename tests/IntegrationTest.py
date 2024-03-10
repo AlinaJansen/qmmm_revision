77505d2fae6461e2a1ab60e1e560e3015255d732
jansea92 <alina.jansen@fu-berlin.de>
2024-03-10 18:26:36 +0100

    working on combining mocking and bfgs

--- tests/IntegrationTest.py
+++ tests/IntegrationTest.py
@@ -7,7 +7,7 @@ gmx2qmmm_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(gmx2qmmm_path)
 
 import gmx2qmmm_execute
-from gmx2qmmm.operations.qmmm_job import execute_g16, execute_gmx
+from gmx2qmmm.operations.qmmm_job import execute_g16, execute_gmx, execute_gmx_communicate
 
 # set working directory
 wd = 'opt_input'
@@ -23,17 +23,20 @@ class IntegrationTest():
     @staticmethod
     def execute_gmx2qmmm_mocking(directory = None):
         if not directory:
-            os.chdir(os.path.join('tests', 'opt_input'))
+            # os.chdir(os.path.join('tests', 'opt_input'))
+            pass
         else:
             os.chdir(directory)
 
         execute_g16_mock = Mock()
         execute_gmx_mock = Mock()
+        execute_gmx_com_mock = Mock()
 
         with patch('gmx2qmmm.operations.qmmm_job.execute_g16', execute_g16_mock):
             with patch('gmx2qmmm.operations.qmmm_job.execute_gmx', execute_gmx_mock):
-                inputFiles = gmx2qmmm_execute.userInputs()
-                gmx2qmmm_execute.gmx2qmmm(inputFiles)
+                with patch('gmx2qmmm.operations.qmmm_job.execute_gmx_communicate', execute_gmx_com_mock):
+                    inputFiles = gmx2qmmm_execute.userInputs()
+                    gmx2qmmm_execute.gmx2qmmm(inputFiles)
         
         return None
     
