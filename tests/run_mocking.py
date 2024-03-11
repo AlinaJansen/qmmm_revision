4d9e14d57e9f1c12e9f0f428b248431e68bff916
jansea92 <alina.jansen@fu-berlin.de>
2024-03-11 11:32:20 +0100

    improve efficiency

--- tests/run_mocking.py
+++ tests/run_mocking.py
@@ -11,25 +11,32 @@ sys.path.append(integration_path)
 from gmx2qmmm.operations.qmmm_job import execute_g16, execute_gmx
 from IntegrationTest import IntegrationTest
 
+mocking = True
+
 '''
 -----------------------------------------------------------------
 Insert path to folder here:
 -----------------------------------------------------------------
 '''
-# directory = os.path.join(os.path.dirname(__file__), 'temp_bfgs_cp24')
-directory = os.path.join(os.path.dirname(__file__), 'bfgs_zerodivision')
+directory = os.path.join(os.path.dirname(__file__), 'temp_bfgs_cp24')
 prof_file = 'profiling_bfgs_cp24.prof'
 txt_file = 'profiling_bfgs_cp24.txt'
 '''
 -----------------------------------------------------------------
 '''
+def run_mocking():
+    IntegrationTest.execute_gmx2qmmm_mocking(directory)
 
-IntegrationTest.execute_gmx2qmmm_mocking(directory)
+def runtime_analysis():
+    cProfile.run("IntegrationTest.execute_gmx2qmmm_mocking(directory)", filename=os.path.join(directory, prof_file))
+    with open(os.path.join(directory, txt_file), "w") as output_file:
+        stats = pstats.Stats(os.path.join(directory, prof_file), stream=output_file)
+        stats.sort_stats('cumtime')
+        stats.print_stats(100)
 
-# cProfile.run("IntegrationTest.execute_gmx2qmmm_mocking(directory)", filename=os.path.join(directory, prof_file))
-# with open(os.path.join(directory, txt_file), "w") as output_file:
-#     stats = pstats.Stats(os.path.join(directory, prof_file), stream=output_file)
-#     stats.sort_stats('cumtime')
-#     stats.print_stats(100)
+    print(f"Profiling analysis saved to {txt_file}")
 
-# print(f"Profiling analysis saved to {txt_file}")
+if mocking:
+    run_mocking()
+else:
+    runtime_analysis()
